68cea5037c64fa7030a6afb35c5bf328212b1e9b
Justin Jones <justin.jones@lumiglobal.com>
2021-09-17 09:44:53 +0100

    base not working

---
+++ main/src/modules/shared/shared.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TestViewComponent } from './test-view/test-view.component';
+
+
+
+@NgModule({
+  declarations: [
+    TestViewComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    TestViewComponent
+  ]
+})
+export class SharedModule { }
---
+++ site1/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { SharedModule } from '../../../main/src/modules/shared/shared.module'
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    SharedModule,
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
